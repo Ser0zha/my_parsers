2056982afc00eb0e9fe8098c13a637ec8828c999
Ser0zha <137228126+Ser0zha@users.noreply.github.com>
2024-07-06 16:51:57 +0300

    Update flashback.py
    1) Исправил текст который парсится (теперь каждое предложение с новой строки)
    2) Создаётся такая директория, какая сейчас и тема (т.е. называние меняется в зависимости от темы текстов)

--- flashback.py
+++ flashback.py
@@ -26,23 +26,22 @@ def fetch_html(base_url: str) -> Response:
 
 
 def main_parser(ready_topics: ResultSet) -> None:
+    def parsing_in_depth(sel, code: int = 0, count: int = 0):
+        link = sel['href']
+        resp = requests.get(url + str(link))
+        bs = BeautifulSoup(resp.content, "html.parser")
+        if code == 0:
+            sel = bs.select('.intro-body-content > .title > a')
+            return sel
+        else:
+            sel = bs.select('.content-text')
+            saves(sel, count, topic.text)
+
     for topic in ready_topics:
         a = parsing_in_depth(topic)
         for count, i in enumerate(a):
             parsing_in_depth(i, code=1, count=count)
         break
-
-
-def parsing_in_depth(sel, code: int = 0, count: int = 0):
-    link = sel['href']
-    resp = requests.get(url + str(link))
-    bs = BeautifulSoup(resp.content, "html.parser")
-    if code == 0:
-        sel = bs.select('.intro-body-content > .title > a')
-        return sel
-    else:
-        sel = bs.select('.content-text')
-        saves(sel, count)
 
 
 def parser(url_addres: str) -> ResultSet:
@@ -56,16 +55,18 @@ def parser(url_addres: str) -> ResultSet:
         exit()
 
 
-def saves(paragraphs: Any, count: int) -> None:
+def saves(paragraphs: Any, count: int, theme: str) -> None:
+    Path(theme).mkdir(parents=True, exist_ok=True)
+
     global filename
     for i in paragraphs:
-        file_os_name = os.path.join("output_2", f'{filename}_{count}.txt')
+        file_os_name = os.path.join(theme, f'{filename}_{count}.txt')
         with open(file_os_name, 'w', encoding='utf-8') as f:
-            f.write(i.get_text(''))
+            txt = i.text.replace(". ", '\n')
+            f.write(txt)
 
 
 def main() -> None:
-    Path("output_2").mkdir(parents=True, exist_ok=True)
     global filename
 
     filename = separation_url(url)
